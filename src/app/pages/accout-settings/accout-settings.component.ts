3cf42ffbbfca1150f367a98801365c04078769af
Jorge Hernandez <hernandezrayojorge@gmail.com>
2020-01-16 07:30:55 -0500

    Desea Guardar los cambios en GitHub

--- src/app/pages/accout-settings/accout-settings.component.ts
+++ src/app/pages/accout-settings/accout-settings.component.ts
@@ -7,19 +7,28 @@ import { Component, OnInit, Inject } from '@angular/core';
   styles: []
 })
 export class AccoutSettingsComponent implements OnInit {
-
+// Con esto (@Inject(DOCUMENT) private  _document ) { }} podemos inyectar una parte del documento, para porteriormente
+// Aplicar un cambio, el ejemplo que vamos hacer es, aplicar y remover una clase
   // tslint:disable-next-line: variable-name
   constructor(@Inject(DOCUMENT) private  _document ) { }
 
   ngOnInit() {
   }
 
-  cambiaColor( tema: string) {
-    console.log(tema);
+  cambiarColor( tema: string, link: any) {
+  this.aplicarCheck( link);
 
-
-    let url = `assets/css/colors/${ tema }.css`;
-    this._document.getElementById('tema').setAttribute('href', url );
-
+  const url: string = `assets/css/colors/${ tema }.css`;
+  this._document.getElementById('tema').setAttribute('href', url );
   }
+  //////////////////////////////////////////////////
+aplicarCheck( link: any ) {
+
+  const selectores: any = document.getElementsByClassName('selector');
+  for ( const ref of selectores ) {
+    ref.classList.remove('working'); { }
+  }
+  link.classList.add('working');
+}
+
 }
